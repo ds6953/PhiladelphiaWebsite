0fa85e99dad8027e90b8d38cf28e25603b20a4b3
Alex Habrat <beastboyfan53@gmail.com>
2022-12-03 16:15:51 -0500

    Minor fix

--- assets/inc/nav.php
+++ assets/inc/nav.php
@@ -3,7 +3,7 @@
             <li><a href="index.php">Home</a></li>
             <li><a href="independence_hall.php">Independence Hall</a></li>
             <li><a href="liberty_bell.php">Liberty Bell</a></li>
-            <li><a href="Penitentiary.php">Penitentiary</a></li>
+            <li><a href="penitentiary.php">Penitentiary</a></li>
             <li><a href="christ_church.php">Christ Church</a></li>
         </ul>
     </nav>
