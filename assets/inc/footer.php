f1abd1f3c47af202f860cc9067ecd8523314282c
Edmund Brotz <erb1851@rit.edu>
2022-12-04 12:18:20 -0500

    liberty bell final content

--- assets/inc/footer.php
+++ assets/inc/footer.php
@@ -2,7 +2,9 @@
         <h2>Sources</h2>
         <nav>
             <ul>
-                <li><a class="external" href="https://en.wikipedia.org/wiki/Philadelphia_County,_Pennsylvania">Wikipedia</a></li>
+                <li><a class="external" href="https://en.wikipedia.org/wiki/Philadelphia_County,_Pennsylvania">Wikipedia - City of Philadelphia</a></li>
+                <li><a class="external" href="https://en.wikipedia.org/wiki/Independence_Hall">Wikipedia - Independence Hall</a></li>
+                <li><a class="external" href="https://en.wikipedia.org/wiki/Liberty_Bell">Wikipedia - Liberty Bell</a></li>
                 <li><a class="external" href="https://www.britannica.com/place/Philadelphia">Britannica</a></li>
         </ul>
         </nav>
