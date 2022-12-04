c82371ed70f3e54a12588800b3459b20c34436e0
Edmund Brotz <erb1851@rit.edu>
2022-12-04 13:09:25 -0500

    prision content modification

--- penitentiary.php
+++ penitentiary.php
@@ -17,18 +17,19 @@
 
     <section class="intro">
       <h3>About</h3>
-      <img src="assets/images/penitentiary.png" alt="penitentiary" width = "500" height = "500">
-      <p></p>
+      <img src="assets/images/penitentiary.png" alt="penitentiary">
+      <p>The Eastern State Penitentiary (ESP) is a former American prison in Philadelphia, Pennsylvania. It is located at 2027 Fairmount Avenue between Corinthian Avenue and North 22nd Street in the Fairmount section of the city, and was operational from 1829 until 1971. The penitentiary refined the revolutionary system of separate incarceration first pioneered at the Walnut Street Jail which emphasized principles of reform rather than punishment.  Notorious criminals such as Al Capone and bank robber Willie Sutton were held inside its innovative wagon wheel design. James Bruno (Big Joe) and several male relatives were incarcerated here between 1936 and 1948 for the alleged murders in the Kelayres massacre of 1934, before they were paroled.  At its completion, the building was the largest and most expensive public structure ever erected in the United States, and quickly became a model for more than 300 prisons worldwide  The prison is currently a U.S. National Historic Landmark, which is open to the public as a museum for tours seven days a week, twelve months a year, 10 am to 5 pm. </p>
     </section>
 
     <section class="characteristics">
         <h3>Characteristics</h3>
         <p>Equally notable for its architecture and mechanical advances, Eastern State served as the structural and ideological model for more than 300 prisons around the world. In architect John Haviland's radial design, cellblocks of the prison extend outward from a central surveillance area.</p>
+        <p>When the Eastern State Penitentiary, or Cherry Hill as it was known at the time, was erected in 1829 in Francisville (the idea of this new prison was created in a meeting held at Benjamin Franklin's house in 1787) it was the largest and most expensive public structure in the country. Its architectural significance first arose in 1821, when British architect John Haviland was chosen to design the building. Haviland found most of his inspiration for his plan for the penitentiary from prisons and asylums built beginning in the 1780s in England and Ireland. He gave the prison a neo-Gothic look to instill fear into those who thought of committing a crime.  These complexes consist of cell wings radiating in a semi or full circle array from a center tower whence the prison could be kept under constant surveillance. The design for the penitentiary which Haviland devised became known as the hub-and-spoke plan which consisted of an octagonal center connected by corridors to seven radiating single-story cell blocks, each containing two ranges of large single cells—8 x 12 feet x 10 feet high—with hot water heating, a water tap, toilet, and individual exercise yards the same width as the cell.</p>
     </section>
 
     <section class="history">
         <h3>History</h3>
-        <p>The Eastern State Penitentiary, also known as ESP, is a former American prison in Philadelphia, Pennsylvania. It is located at 2027 Fairmount Avenue between Corinthian Avenue and North 22nd Street in the Fairmount section of the city, and was operational from 1829 until 1971. The penitentiary refined the revolutionary system of separate incarceration first pioneered at the Walnut Street Jail which emphasized principles of reform rather than punishment.</p>
+        <p>The solitary confinement system eventually collapsed due to overcrowding problems. By 1913, Eastern State officially abandoned the solitary system and operated as a congregate prison until it closed in 1970. Eastern State was briefly used to house city inmates in 1971 after a riot at Holmesburg Prison Al Capone's cell.  The remains of the barber shop.  The prison was one of the largest public-works projects of the early republic, and was a tourist destination in the 19th century. Notable visitors included Charles Dickens and Alexis de Tocqueville, and later notable inmates included Willie Sutton and Al Capone in 1929. Visitors spoke with prisoners in their cells, proving that inmates were not isolated, though the prisoners themselves were not allowed to have visits with family or friends during their stay.  Most of the early prisoners were petty criminals incarcerated for various robbery and theft charges (muggers, pickpockets, purse-snatchers, burglars, etc.) and the first-time offenders often served two years</p>
         
         <p>Notorious criminals such as Al Capone and bank robber Willie Sutton were held inside its innovative wagon wheel design. James Bruno (Big Joe) and several male relatives were incarcerated here between 1936 and 1948 for the alleged murders in the Kelayres massacre of 1934, before they were paroled. At its completion, the building was the largest and most expensive public structure ever erected in the United States, and quickly became a model for more than 300 prisons worldwide.</p>
     </section>
@@ -37,7 +38,8 @@
     <section class="location">
         <h3>Location</h3>
         <img src="assets/images/penitentiarymap.png" alt="Penintentiary">
-        <p>Located in the Fairmount section of Philadelphia. The address is 30 Institution Road Napanoch, New York</p>
+        <p>Located in the Fairmount section of Philadelphia. The address is 2027 Fairmount Avenue
+Philadelphia, PA 19130</p>
     </section>
    
     <?php include "assets/inc/footer.php"; ?>
