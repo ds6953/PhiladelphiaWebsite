b6d812221f3ec1202c4e788019d21ba6d3f50678
Edmund Brotz <erb1851@rit.edu>
2022-12-04 13:37:37 -0500

    christ church final content

--- assets/inc/footer.php
+++ assets/inc/footer.php
@@ -5,6 +5,8 @@
                 <li><a class="external" href="https://en.wikipedia.org/wiki/Philadelphia_County,_Pennsylvania">Wikipedia - City of Philadelphia</a></li>
                 <li><a class="external" href="https://en.wikipedia.org/wiki/Independence_Hall">Wikipedia - Independence Hall</a></li>
                 <li><a class="external" href="https://en.wikipedia.org/wiki/Liberty_Bell">Wikipedia - Liberty Bell</a></li>
+                <li><a class="external" href="https://en.wikipedia.org/wiki/Eastern_State_Penitentiary">Wikipedia - Penitentiary</a></li>
+                <li><a class="external" href="https://en.wikipedia.org/wiki/Christ_Church,_Philadelphia">Wikipedia - Christ Church</a></li>
                 <li><a class="external" href="https://www.britannica.com/place/Philadelphia">Britannica</a></li>
         </ul>
         </nav>
